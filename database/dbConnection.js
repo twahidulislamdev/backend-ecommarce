0a4ff5d17449c4f36358a753fd6245a122456c44
Twahidul Islam <twahidulislam2005@gmail.com>
2026-01-08 12:41:46 +0600

    DB file config

---
+++ database/dbConnection.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+// function dbConnection() {
+//   mongoose
+//     .connect(
+//       `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@backendecommarce.a4vhkvw.mongodb.net/${process.env.DB_NAME}?appName=backendecommarce`
+//     )
+//     .then(() => console.log("DB Connected!"));
+// }
+
+function dbConnection() {
+  mongoose
+    .connect(
+      `${process.env.DB_URL}`
+    )
+    .then(() => console.log("DB Connected!"));
+}
+module.exports = dbConnection;
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const dbConnection = require("./database/dbConnection");
 const app = express();
 const port = 3000;
+require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+dbConnection();
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
